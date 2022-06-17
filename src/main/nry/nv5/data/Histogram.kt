d4a8cd634b6057a6f1e583b97040022105c7625c
Henry G <github.nry@icloud.com>
2022-06-17 09:59:31 +0100

    Refactor handleRequest to return JSON from a data class

---
+++ src/main/nry/nv5/data/Histogram.kt
@@ -0,0 +1,3 @@
+package nry.nv5.data
+
+data class Histogram (val data: List<Double>, val info: String, val size: Int)
--- src/main/nry/nv5/lambda/Handler.kt
+++ src/main/nry/nv5/lambda/Handler.kt
@@ -1,23 +1,28 @@
 package nry.nv5.lambda
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.google.gson.Gson
+import nry.nv5.data.Histogram
 
 
-class Handler: RequestHandler<Map<String, Object>, String> {
-    override fun handleRequest(event: Map<String, Object>, ctx: Context): String {
+class Handler: RequestHandler<Map<String, Any>, String> {
+    var gson = Gson()
+    override fun handleRequest(event: Map<String, Any>, ctx: Context): String {
         val n = event.let {
             it["quantity"]?.toString()?.toInt() ?: 1000
         }
 
-        var rnds: Int;
-        val nums = mutableListOf<Int>();
+        var rnds: Int
+        val nums = mutableListOf<Int>()
         for (i in 1..n) {
             rnds = (0..15).random()
             nums.add(rnds)
         }
 
-        return "{\"data\": " + org.json.JSONArray(nums) + "}"
+        val histogramData = Histogram(nums.toList() as List<Double>, "", nums.size)
+
+        return gson.toJson(histogramData)
     }
 
 }
